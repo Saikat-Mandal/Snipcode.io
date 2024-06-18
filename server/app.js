9b10c54b77b9d9f4d0ec6d6e4ec05bdb42639d4a
Saikat Mandal <msaikat022@gmail.com>
2024-06-18 23:36:32 +0530

    changed cors policy

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200,
   })
